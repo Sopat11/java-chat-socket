9a94d38c22050676d19dd6be68d4b3e8d97b9309
Patryk Sosiński <patryk.sosinski1@gmail.com>
2022-04-02 15:12:41 +0200

    Reading history of current channel

--- src/main/java/it/sosinski/history/HistoryService.java
+++ src/main/java/it/sosinski/history/HistoryService.java
@@ -1,26 +1,42 @@
 package it.sosinski.history;
 
 import it.sosinski.channel.Channel;
+import it.sosinski.chatworker.ChatWorker;
+import lombok.extern.java.Log;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
+import java.util.logging.Level;
 
+@Log
 public class HistoryService {
+
+    private static final String HISTORY_PATH = "history\\";
 
     public static void saveMessageToTxtFile(String text, Channel channel) {
         new Thread(() -> {
-            PrintWriter printWriter = null;
-            try {
-                FileWriter fileWriter = new FileWriter(channel.getName() + ".txt", true);
-                printWriter = new PrintWriter(fileWriter);
+            String filePath = HISTORY_PATH + channel.getName() + ".txt";
+            try (FileWriter fileWriter = new FileWriter(filePath, true);
+                 PrintWriter printWriter = new PrintWriter(fileWriter)) {
+
                 printWriter.write(text + "\n");
             } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                if (printWriter != null) {
-                    printWriter.close();
+                log.log(Level.SEVERE, "Saving message to history failed: " + e.getMessage());
+            }
+        }).start();
+    }
+
+    public static void readHistory(ChatWorker chatWorker) {
+        new Thread(() -> {
+            Channel channel = chatWorker.getCurrentChannel();
+            String filePath = HISTORY_PATH + channel.getName() + ".txt";
+
+            try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+                String text;
+                while ((text = reader.readLine()) != null) {
+                    chatWorker.sendServerMsg(text);
                 }
+            } catch (IOException e) {
+                log.log(Level.SEVERE, "Reading message from history failed: " + e.getMessage());
             }
         }).start();
     }
